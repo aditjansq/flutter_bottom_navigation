2541d8ded9caa26a2df95483d885919e1b203b63
aditjansq <132744602+aditjansq@users.noreply.github.com>
2024-12-02 11:32:43 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_button_application
+
+A new Flutter project.
---
+++ lib/data/user_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_bottom_navigation/models/user.dart';
+
+var userList = [
+  User(
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    password: '123456',
+    profilePicture: 'images/profile_picture.jpg',
+  )
+];
